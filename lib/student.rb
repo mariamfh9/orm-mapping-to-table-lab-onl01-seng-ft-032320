8d51445abd6c508c7906b70202c1fe4dfcc56107
Mariam Haider <mfh.haider@gmail.com>
2020-04-21 20:57:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,17 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id)
+    @name = name
+    @grade = grade
+    @id = id
+  end 
+  
+  def create_table
+    
+  end 
   
 end
